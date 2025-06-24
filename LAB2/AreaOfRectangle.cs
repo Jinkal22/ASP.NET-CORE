18bbc99fb276ac375e62657acf0f59b585c30d68
Jinkal22 <141735426+Jinkal22@users.noreply.github.com>
2025-06-24 08:38:10 +0530

    Add files via upload

---
+++ LAB2/AreaOfRectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class AreaOfRectangle
+    {
+        double length;
+        double width;
+        public AreaOfRectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        public double CalculateArea()
+        {
+            return length * width;
+        }
+    }
+}
